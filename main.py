ba7041ffe569188cdfbf05931a8821809e9b5ee3
SGman98 <sgarnicaq@unal.edu.co>
2021-04-13 01:08:40 -0500

    Linker module has been implemented Closes #5
    Co-authored-by: Harrison Pinto <HASPIMA@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from cli.cli import runcli
+from assembler.assembler import assemble_from_file
+from linker.linker import link
 
 if __name__ == '__main__':
-	runcli()
+	assemble_from_file('examples/euclides.z80.asm')
+	link('output/test.z80.loc','output/test.z80.loc.tag')

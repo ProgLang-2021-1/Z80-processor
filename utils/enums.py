c9a2b566bf3b24d4bc40773d08914e6749d5b1d5
Harrison <harrison.pinto@d4d.com.co>
2021-04-12 23:46:50 -0500

    Some enums and file related functions were added
    Co-authored-by: Sebastian Garnica <SGman98@users.noreply.github.com>

--- utils/enums.py
+++ utils/enums.py
@@ -14,3 +14,23 @@ class Register(Enum):
 	HL = '10'
 	SP = '11'
 	AF = '11'
+
+
+class Flags(Enum):
+	S = 1 << 7
+	Z = 1 << 6
+	H = 1 << 4
+	P = 1 << 2
+	N = 1 << 1
+	C = 1 << 0
+
+
+class FlagCC(Enum):
+	NZ = '000'
+	Z = '001'
+	NC = '010'
+	C = '011'
+	PO = '100'
+	PE = '101'
+	P = '110'
+	M = '111'
